70073add780a3f5b99c90ea2e8ab4d4ffe167c41
phon1e <wiroon2543@gmail.com>
2021-01-05 12:11:51 +0700

    commit_test

---
+++ tcp_server.js
@@ -0,0 +1,22 @@
+const { data } = require('autoprefixer');
+const console = require('console');
+var net = require('net');
+
+var HOST = '127.0.0.1';
+var PORT = 6969;
+
+var server = net.createServer();
+server.listen(PORT, HOST);
+
+server.on('connection', function(sock) {
+    console.log('CONNECT: ' + sock.remoteAddress + ':'+ sock.remotePort);
+
+    sock.on('data', function(data) {
+        console.log('DATA' + sock.remoteAddress + ': ' + data);
+        sock.write('You said "' + data +'"');
+    });
+
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+    });
+});
